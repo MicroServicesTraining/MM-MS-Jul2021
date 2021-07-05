af53c680deb192731d31b5c800e1f5da1f68fb6b
sekharbabu4patnam <babuthetrainer@gmail.com>
2021-07-05 07:47:49 +0530

    Initial Commit for SCM Vendor service

---
+++ mm-scm-vendor/src/main/java/com/mm/scm/vendor/MmScmVendorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mm.scm.vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmScmVendorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmScmVendorServiceApplication.class, args);
+	}
+
+}
